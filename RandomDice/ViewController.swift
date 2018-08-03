f0720c66d5137238c3c2679491a923f8934cd17f
Xavier <chenchopan@gmail.com>
2018-08-03 17:40:39 +0800

    Initial Commit

---
+++ RandomDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomDice
+//
+//  Created by Xavier Chen [MIGOTP] on 2018/8/3.
+//  Copyright © 2018年 Xavier Chen [MIGOTP]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
