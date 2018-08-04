917d6ff0ee07d4686f2ba63e3fe5c7b598024a93
Xavier <chenchopan@gmail.com>
2018-08-04 15:07:34 +0800

    RandomDice first commit

--- RandomDice/ViewController.swift
+++ RandomDice/ViewController.swift
@@ -9,9 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var diceImageCollection: [UIImageView]!
+    
+    var diceArray = ["dice-1","dice-2","dice-3","dice-4","dice-5","dice-6"]
+    
+    @IBOutlet weak var selectSegment: UISegmentedControl!
+    @IBAction func buttonChange(_ sender: Any) {
+        changeDice()
+    }
+    
+    func changeDice() {
+        if selectSegment.selectedSegmentIndex == 0 {
+            diceArray.shuffle()
+            for (index, i) in diceArray.enumerated() {
+                diceImageCollection[index].image = UIImage.init(named: i)
+            }
+        } else {
+            for n in 0...5 {
+                let diceNum = diceArray[Int.random(in: 0...5)]
+                diceImageCollection[n].image = UIImage.init(named: diceNum)
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        changeDice()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
